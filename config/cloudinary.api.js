af4bec9887ecebb337f8bc951f0f38846e8a977c
Aurelie Lebec <aurelie.lebec@gmail.com>
2018-02-18 22:08:18 -0600

    [webpack]

--- config/cloudinary.api.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cloudName: 'fido',
-  APIkey: '738253122858585',
-  APIsecret: 'yxmcokBWzBSjjwoSPnp9XCKmkZ4',
-  env: 'CLOUDINARY_URL=cloudinary://738253122858585:yxmcokBWzBSjjwoSPnp9XCKmkZ4@fido',
-};
--- config/googlemaps.api.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  token: 'AIzaSyBfz7Y7C-7emBWPSEi925MBpeXLRcL-Jzw',
-};
